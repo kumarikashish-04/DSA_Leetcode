9faf3315a9eb05ef38d6f212b7ad4f0ffb42b8df
Kashish Kumari <kashish87897231@gmail.com>
2026-08-03 10:46:45 +0530

    Time: 8 ms (20.22%), Space: 47 MB (12.02%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,28 @@
 class Solution {
+    int[][]dp;
+    int solve(int i, int j, int[] piles) {
+        if (i > j)
+            return 0;
+        if (i == j)
+            return piles[i];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int as = piles[i] + Math.min(solve(i + 2, j, piles), solve(i + 1, j - 1, piles));
+        int bs = piles[j] + Math.min(solve(i, j - 2, piles), solve(i + 1, j - 1, piles));
+        dp[i][j]=Math.max(as, bs);
+        return dp[i][j];
+    }
+
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int sum = 0;
+        dp= new int[n][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        for (int i = 0; i < n; i++) {
+            sum += piles[i];
+        }
+        int score = solve(0, n - 1, piles);
+        return score * 2 >= sum;
     }
 }
