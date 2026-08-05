fefac9d5f348b664a75d72d6bce2b65cfc430b4d
Kashish Kumari <kashish87897231@gmail.com>
2026-08-05 11:24:12 +0530

    Time: 0 ms (100%), Space: 43.3 MB (81.84%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -5,28 +5,18 @@ class Solution {
     int[][] dp;
 
     int up(int[][] grid, int i, int j) {
-
-        // Out of bounds
         if (i >= n || j >= m) {
             return 0;
         }
-
-        // Obstacle
         if (grid[i][j] == 1) {
             return 0;
         }
-
-        // Destination
         if (i == n - 1 && j == m - 1) {
             return 1;
         }
-
-        // Already computed
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-
-        // Move Down + Move Right
         return dp[i][j] = up(grid, i + 1, j) + up(grid, i, j + 1);
     }
 
@@ -34,8 +24,6 @@ class Solution {
 
         n = grid.length;
         m = grid[0].length;
-
-        // If start or destination is blocked
         if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
             return 0;
         }
