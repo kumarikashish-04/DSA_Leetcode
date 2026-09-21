79e9dd41be3afc233b36e037d47898e62cfc6aa2
Kashish Kumari <kashish87897231@gmail.com>
2026-09-21 23:07:39 +0530

    Time: 14 ms (67.31%), Space: 93.5 MB (38.46%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -3,16 +3,15 @@ class Solution {
     public long[] resultArray(int[] nums, int k) {
         int n = nums.length;
         long[] result = new long[k];
-        long[] dp = new long[k]; // Initial state: no elements have been processed, so no non-empty subarray exists.
-
+        long[] dp = new long[k]; 
+        
         for (int i = 0; i < n; i++) {
-            long[] ndp = new long[k]; // Current-layer state (rolling array).
+            long[] ndp = new long[k]; 
             ndp[nums[i] % k]++;
             for (int r = 0; r < k; r++) {
                 ndp[(int) (((long) r * nums[i]) % k)] += dp[r];
             }
-            dp = ndp; // Update the state.
-            // Accumulate the answer.
+            dp = ndp; 
             for (int r = 0; r < k; r++) {
                 result[r] += dp[r];
             }
